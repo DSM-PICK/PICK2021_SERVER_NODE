5d354a0f57e652fc1a3303748d37f6aa1f41999d
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-27 11:56:55 +0900

    [UPDATE] import typeormmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
