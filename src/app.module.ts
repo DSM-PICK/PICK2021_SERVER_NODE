4e20b87a8c4075982725dd66b9fbb76569f5ae26
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-29 18:04:16 +0900

    [UPDATE] addAttendanceModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-  imports: [TypeOrmConfigModule],
+  imports: [TypeOrmConfigModule, AttendanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
