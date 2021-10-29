e0c620d185ac380e621aa544663b841eef7cbc74
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-29 17:59:39 +0900

    [Delte] attendanceRepository

--- src/entity/attendence/attendence.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Attendence } from './attendence.entity';
-
-@EntityRepository(Attendence)
-export class AttendenceRepository extends Repository<Attendence> {}
