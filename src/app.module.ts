cf9c9851ef0bd5663e314abffd6ec36ee377c6dd
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-29 18:07:59 +0900

    [UPDATE] add LocationModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
 import { AttendanceModule } from './attendance/attendance.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [TypeOrmConfigModule, AttendanceModule],
+  imports: [TypeOrmConfigModule, AttendanceModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
