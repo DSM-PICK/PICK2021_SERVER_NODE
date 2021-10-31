aac0f36499161c7853e26afc12fc8cc6618142fc
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-31 23:46:49 +0900

    [UPDATE] class 이름 수정

--- src/repositories/attendance.repository.ts
+++ src/repositories/attendance.repository.ts
@@ -2,4 +2,5 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Attendance } from '../entities/attendance/attendance.entity';
 
 @EntityRepository(Attendance)
-export class AttendanceRepository extends Repository<Attendance> {}
+export class AttendanceRepository extends Repository<Attendance> {
+}
