dfecbbbfe2d3ac5c80a76eed08b081e5fc2fa137
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-01 08:33:31 +0900

    [ADD] attendanceModule

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AttendanceService } from './attendance.service';
 import { AttendanceController } from './attendance.controller';
+import { AttendanceRepository } from 'src/repositories/attendance.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AttendanceRepository])],
   controllers: [AttendanceController],
   providers: [AttendanceService]
 })
