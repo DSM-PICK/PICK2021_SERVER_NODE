8f8049964f868ca4e07cb830a62f4022e146a9c0
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-31 23:48:48 +0900

    [ADD] create-locationDTO

--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,1 +1,4 @@
-export class CreateLocationDto {}
+import { OmitType } from "@nestjs/mapped-types";
+import { Location } from "src/entities/location/location.entity";
+
+export class CreateLocationDto extends OmitType(Location, ['location_id']){}
