ad341eb59410acc0f0a042fbc8f4eced7ab224cd
ayeonChoi <201120cay@dsm.hs.kr>
2021-10-31 23:51:17 +0900

    [UPDATE] LocationRepository 추가

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
+import { LocationRepository } from 'src/repositories/location.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LocationRepository])],
   controllers: [LocationController],
   providers: [LocationService]
 })
