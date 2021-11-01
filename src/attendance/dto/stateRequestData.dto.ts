962dbdbc2c1338660c4f66eac53797e32d52b757
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-01 10:50:37 +0900

    [UPDATE] length 추가

--- src/attendance/dto/stateRequestData.dto.ts
+++ src/attendance/dto/stateRequestData.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator";
+import { IsString, Length } from "class-validator";
 
 export class StateReqData{
     @IsString()
+    @Length(20)
     state: string;
 }
