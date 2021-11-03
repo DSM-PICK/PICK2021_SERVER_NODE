fd94f15b4aba1221e76f935aa93cd5e5323d16f9
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-03 23:44:00 +0900

    [UPDATE] LocationRepository

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -2,17 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Location } from '../entities/location/location.entity';
+import { LocationRepository } from '../repositories/location.repository';
 
 @Injectable()
 export class LocationService {
 
   constructor(
-    @InjectRepository(Location) private readonly location: Repository<Location>
+    @InjectRepository(Location) private readonly location: Repository<LocationRepository>
   ){}
-
-  // public getLocation(){
-  //   return this.location.findAll();
-  // }
   
   async deleteOne(id:number): Promise<void>{
     try{
