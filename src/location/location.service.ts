6818ff6a5dfef6c3cc8961d1c431f019bfff4f4a
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-15 13:27:30 +0900

    [Modify]locationService

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,29 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { notFoundLocationIdException } from 'src/exception/exception.location';
 import { Location } from '../entities/location/location.entity';
 import { LocationRepository } from '../repositories/location.repository';
+import { LocationReqDto } from './dto/location-req.dto';
+import { ModifyLocationDto } from './dto/modifyLocation.dto';
 
 @Injectable()
 export class LocationService {
-
   constructor(
-    @InjectRepository(Location) private readonly location: Repository<LocationRepository>
+    @InjectRepository(Location) private readonly locationRepository: LocationRepository
   ){}
-  
-  async deleteOne(id:number): Promise<void>{
-    try{
-      await this.location.delete(id);
-    }catch(e){
-      console.log('error')
-    }
+
+  async getLocationlist(){
+    return await this.locationRepository.getLocationlist()
   }
 
-  async update(id: number, updateData): Promise<void>{
-    try{
-      await this.location.update(id, updateData);
-    }catch(e){
-      console.log('error');
+  async addLocation(dto: LocationReqDto){
+    const location = new Location();
+
+    location.floor = dto.floor;
+    location.name = dto.name;
+    location.priority = dto.priority;
+    return await this.locationRepository.save(location);
+    
+  }
+
+  async deleteLocation(location_id: number){
+    if(!(await this.locationRepository.checkExistLocation(location_id))){
+      throw notFoundLocationIdException;
     }
+    return await this.locationRepository.deleteLocation(location_id);
+  }
+
+  async updateLocation(modifyLocationData: ModifyLocationDto){
+    return await this.locationRepository.updateLocation(modifyLocationData);
   }
 }
