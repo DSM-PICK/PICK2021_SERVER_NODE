f5dd5e84064f8523ca719b4ce84fd5378dcb6646
ayeonChoi <201120cay@dsm.hs.kr>
2021-11-15 13:28:55 +0900

    [UPDATE]locationRepository
    getLocationList(), checkExistLocation(), deleteLocation(), updateLocation() 추가

--- src/repositories/location.repository.ts
+++ src/repositories/location.repository.ts
@@ -1,5 +1,47 @@
+import { ModifyLocationDto } from 'src/location/dto/modifyLocation.dto';
 import { EntityRepository, Repository } from 'typeorm';
 import { Location } from '../entities/location/location.entity'
 
 @EntityRepository(Location)
-export class LocationRepository extends Repository<Location> {}
+export class LocationRepository extends Repository<Location> {
+
+    public async getLocationlist(){
+        return this.createQueryBuilder('location')
+        .innerJoin('location.major_id', 'major_id')
+        .select('location.id', 'id')
+        .select('location.floor', 'floor')
+        .select('location.priority','priority')
+        .select('location.name', 'name')
+    }
+    public async checkExistLocation(location_id: number): Promise<boolean>{
+        const location = await this.createQueryBuilder('location')
+        .select('location.location_id', 'location_id')
+        .where('post.post_id = :location_id', {location_id: location_id})
+        .getOne();
+        if(location){
+            return true;
+        }
+        return false;
+    }
+
+    public async deleteLocation(location_id:number){
+        return this.createQueryBuilder('location')
+        .delete()
+        .from(Location)
+        .where('location_id = :location_id', { location_id: location_id})
+        .execute();
+    }
+
+    public async updateLocation(modifyLocationData: ModifyLocationDto){
+        let floor = modifyLocationData.floor;
+        let name = modifyLocationData.name;
+        let priority = modifyLocationData.priority;
+
+        return this.createQueryBuilder()
+            .update(Location)
+            .set({ floor: floor, name: name, priority: priority})
+            .where('location.location_id = :location_id, {location_id: location}')
+            .execute();
+
+    }
+}
