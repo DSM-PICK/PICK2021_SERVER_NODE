11578c2f40cd9e50bb2cce1525ea8e0cb50edc67
ayeonChoi <201120cay@dsm.hs.kr>
2021-12-10 13:27:12 +0900

    [UPDATE] affliatedAfterSchoolRepository

--- src/repositories/affliatedAfterSchool.repository.ts
+++ src/repositories/affliatedAfterSchool.repository.ts
@@ -1,5 +1,5 @@
+import { AffliatedAfterSchool } from 'src/entities/affliatedAfterShool/affliatedAfterSchool.entity';
 import { EntityRepository, Repository } from 'typeorm';
-import { AffliatedAfterSchool } from '../entities/affliated/affliated.entity';
 
 @EntityRepository(AffliatedAfterSchool)
 export class AffliatedAfterSchoolRepository extends Repository<AffliatedAfterSchool> {}
