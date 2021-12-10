9c13d76ef5b8c6094066b9658dd8edacbca74773
ayeonChoi <201120cay@dsm.hs.kr>
2021-12-10 09:54:46 +0900

    [UPDATE] afterSchoolEntity

--- src/entities/afterschool/afterschool.entity.ts
+++ src/entities/afterschool/afterschool.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn,PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn,OneToOne,PrimaryGeneratedColumn } from 'typeorm';
 import { Teacher } from '../teacher/teacher.entity';
 import { Location } from '../location/location.entity';
 
@@ -13,9 +13,9 @@ export class AfterSchool {
   @Column({ length: 3 })
   day: string;
 
-  @JoinColumn({ name: 'teacher_id'})
-  teacher: Teacher;
+  @OneToOne(type => Teacher, teacher => teacher.teacher_id)
+  teacher_id: number;
 
-  @JoinColumn({ name: 'location_id'})
-  location: Location;
+  @OneToOne(type => Location, location => location.location_id)
+  location_id: number;
 }
