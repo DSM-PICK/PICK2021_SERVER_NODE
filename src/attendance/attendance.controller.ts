84ff6657c4a4b1ca97d9474818eb7ff59da6525f
ayeonChoi <201120cay@dsm.hs.kr>
2021-12-12 18:13:09 +0900

    [UPDATE]attendanceController

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,27 +1,36 @@
-import { Body, Controller, Get, Param, Patch, Post} from '@nestjs/common';
+import { Body, Controller, Delete, Get,  Param,  Patch } from '@nestjs/common';
 import { Attendance } from 'src/entities/attendance/attendance.entity';
 import { AttendanceService } from './attendance.service';
 import { AttendanceReqData } from './dto/attendanceRequest.dto';
-import { AttendanceResData } from './dto/attendanceResponse.dto';
 import { StateReqData } from './dto/stateRequestData.dto';
+
 
 @Controller('attendance')
 export class AttendanceController {
-  constructor(private readonly attendanceService: AttendanceService) {
+  constructor(private readonly attendanceService: AttendanceService) {}
+
+  @Get()
+  public async getAttendance(){
+    return await this.attendanceService.getAttendance();
+  }
+
+  @Delete('/attendance')
+  public async deleteAttendance(@Body() attendance_id: number){
+    await this.attendanceService.deleteAttendance(attendance_id);
+    return { status: 204, message: 'success'};
   }
 
   @Patch()
-  updateAttendance(@Body() attendanceId: number, AttendanceReqData: AttendanceReqData){
-    return this.attendanceService.updateState(attendanceId, AttendanceReqData);
+  public async updateAttendance(@Body() attendanceReqData: AttendanceReqData){
+    await this.attendanceService.updateAttendance(attendanceReqData);
+    return { status: 204, message: 'success'}
   }
-
-  @Get()
-  getAll():Attendance[]{
-    return this.attendanceService.getAll();
-  }
-
+  
   @Patch('/state')
-  updateState(@Body()id, stateReqData:StateReqData){
-    return this.attendanceService.updateState(id, stateReqData );
+  public async updateState(@Body() stateReqData: StateReqData){
+    await this.attendanceService.updateState(stateReqData);
+    return { status: 204, message: 'success'}
   }
 }
+
+
