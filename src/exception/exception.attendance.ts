061528ee86f4f89c17550cbaba93e9fe64fcaa24
ayeonChoi <201120cay@dsm.hs.kr>
2021-12-12 18:12:34 +0900

    [ADD] exceptionAttendance

---
+++ src/exception/exception.attendance.ts
@@ -0,0 +1,12 @@
+import {
+    BadRequestException,
+    NotFoundException,
+    ConflictException,
+  } from '@nestjs/common';
+  
+  export const badRequestException = new BadRequestException();
+  
+  export const ExistAttendanceError = new ConflictException('Attendance already exist');
+  export const notFoundAttendanceIdException = new NotFoundException('attendanceId is not founded'); 
+  
+ 
