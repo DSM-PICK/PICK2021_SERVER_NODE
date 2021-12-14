de3f5569839df1a66306faa9e44d88d6a76c6c6b
ayeonChoi <201120cay@dsm.hs.kr>
2021-12-14 11:57:23 +0900

    [MODIFY]locationModule

--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -3,9 +3,10 @@ import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
 import { LocationRepository } from 'src/repositories/location.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Location } from 'src/entities/location/location.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LocationRepository])],
+  imports: [TypeOrmModule.forFeature([Location])],
   controllers: [LocationController],
   providers: [LocationService]
 })
