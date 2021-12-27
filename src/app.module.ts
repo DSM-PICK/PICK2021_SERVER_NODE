1270bbb41b660f8bb30cc65def6d8cb98ad7bc24
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2021-12-27 09:37:51 +0900

    [UPDATE]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmConfigModule } from './typeorm/typeorm-config.module';
-import { AttendanceModule } from './attendance/attendance.module';
-import { LocationModule } from './location/location.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmConfigModule } from "./typeorm/typeorm-config.module";
+import { AttendanceModule } from "./attendance/attendance.module";
+import { LocationModule } from "./location/location.module";
 
 @Module({
-  imports: [
-    TypeOrmConfigModule, 
-    AttendanceModule, 
-    LocationModule],
+  imports: [TypeOrmConfigModule, AttendanceModule, LocationModule],
   controllers: [AppController],
   providers: [AppService],
 })
