43838c2511f312998c61fe67d0956ebd026fb7cf
ayeonChoi <201120cay@dsm.hs.kr>
2022-01-01 16:12:07 +0900

    Update ModifyLocationDto

--- src/location/dto/modifyLocation.dto.ts
+++ src/location/dto/modifyLocation.dto.ts
@@ -1,5 +1,12 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class ModifyLocationDto{
+    @IsNumber()
     floor: number;
+
+    @IsNumber()
     priority: number;
+
+    @IsString()
     name: string;
 }
