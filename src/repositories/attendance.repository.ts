39fc872c0f27961f5bf81854e4d369c653e98a58
ayeonChoi <201120cay@dsm.hs.kr>
2022-01-05 17:11:48 +0900

    Update updateAttendance

--- src/repositories/attendance.repository.ts
+++ src/repositories/attendance.repository.ts
@@ -34,7 +34,7 @@ export class AttendanceRepository extends Repository<Attendance> {
         .addSelect('tbl_attendance.reason')
         .leftJoinAndSelect('tbl_attendance.student', 'student')
         .leftJoinAndSelect('tbl_attendance.director', 'id')
-        .leftJoinAndSelect('tbl_attendance.teacher', 'name')
+        .leftJoinAndSelect('tbl_attendance.teacher', 'teacher')
         .getMany()
     }
 }
