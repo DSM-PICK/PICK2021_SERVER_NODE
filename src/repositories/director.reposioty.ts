83e407a09dd80dd3d20f0867be1a0be2614a3087
leeseojune53 <sung07288346@gmail.com>
2022-05-13 00:24:05 +0900

    ⚡️ :: queryDirectorByScheduleAndFloor

--- src/repositories/director.reposioty.ts
+++ src/repositories/director.reposioty.ts
@@ -2,4 +2,14 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Director } from '../entities/director.entity';
 
 @EntityRepository(Director)
-export class DirectorRepository extends Repository<Director> {}
+export class DirectorRepository extends Repository<Director> {
+  public async queryDirectorByScheduleAndFloor(
+    schedule_id: number,
+    floor: number,
+  ) {
+    return await this.createQueryBuilder('tbl_director')
+      .where('tbl_director.schedule_id= :id', { id: schedule_id })
+      .andWhere('tbl_director.floor= :floor', { floor: floor })
+      .getOne();
+  }
+}
