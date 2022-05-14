8225bce976bdbd134ab929360dae8b3fa8e1e4fc
ayeonChoi <201120cay@dsm.hs.kr>
2022-05-14 21:54:10 +0900

    :hammer: :: selfStudyStudentInfo&Attendance 쿼리 추가

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -39,4 +39,43 @@ export class StudentRepository extends Repository<Student> {
       );
     });
   }
+
+  public async querySelfStudyStudentInfo(
+    locationId: number,
+  ): Promise<StudentInfo[]> {
+    return (
+      await this.createQueryBuilder('tbl_student')
+        .select(['tbl_student.id', 'tbl_student.name', 'tbl_student.gcn'])
+        .where('tbl_student.location_id = :locationId', {
+          locationId: locationId,
+        })
+        .getMany()
+    ).map((student) => {
+      return new StudentInfo(student.id, student.name, student.gcn);
+    });
+  }
+
+  public async querySelfStudyStudentAttendance(
+    directorId: number,
+    locationId: number,
+  ): Promise<StudentAttendance[]> {
+    return (
+      await this.query(
+        `SELECT A.id, ta.period, tl.name, ta.state FROM pick2021.tbl_student A
+        LEFT JOIN tbl_attendance ta on A.id = ta.student_id AND director_id = ?
+        LEFT JOIN tbl_location tl on A.location_id = tl.id
+        WHERE A.location_id = ?`,
+        [directorId, locationId],
+      )
+    ).map((attendance) => {
+      const locationName =
+        attendance.state == '외 출' ? attendance.getLocationName() : null;
+      return new StudentAttendance(
+        attendance.id,
+        attendance.period,
+        locationName,
+        attendance.state,
+      );
+    });
+  }
 }
