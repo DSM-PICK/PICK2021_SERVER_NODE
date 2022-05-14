6a2ce5ef85e96e0357d0a019f6af1020045d437e
leeseojune53 <sung07288346@gmail.com>
2022-05-14 17:49:07 +0900

    📑 :: StudentInfo

---
+++ src/repositories/vo/student/studentInfo.vo.ts
@@ -0,0 +1,11 @@
+export class StudentInfo {
+  constructor(id: number, name: string, gcn: string) {
+    this.id = id;
+    this.name = name;
+    this.gcn = gcn;
+  }
+
+  id: number;
+  name: string;
+  gcn: string;
+}
